3eb2b97100e7b84de8a0006c7a24dc0eaaa71524
Jorge Palacios (reefaktor) <reefaktor@gmail.com>
2019-01-21 21:17:11 -0300

    Initial commit with a couple behaviors * Basic Player Controller * Basic AI Arrive behavior

---
+++ Assets/AIShooter/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Vector3 _direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+        _direction.Normalize();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(_direction * _speed * Time.deltaTime, Space.World);
+    }
+    
+}
