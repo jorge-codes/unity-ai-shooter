3031e6d225d8315d3e83243c4808e73da885c920
Jorge Palacios <reefaktor@gmail.com>
2019-02-04 21:30:18 -0300

    Improved camera following movement and more * Improved camera movement when following * Created level 001

---
+++ Assets/AIShooter/Scripts/EndLevelController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EndLevelController : MonoBehaviour
+{
+
+    public string objectTag;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(objectTag)) return;
+        
+        Application.Quit();
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
--- Assets/AIShooter/Scripts/FollowingCamera.cs
+++ Assets/AIShooter/Scripts/FollowingCamera.cs
@@ -5,17 +5,21 @@ public class FollowingCamera : MonoBehaviour
 {
     [SerializeField] protected float _speed;
     [SerializeField] protected Transform _target;
+    [SerializeField] protected float _actionRadius;
     [SerializeField] protected float _stopRadius;
+    protected bool _isMoving;
+    protected float[] _radii;
     protected Vector3 _relativePosition;
-//    protected Camera _camera;
 
     protected float _distance;
-//    protected Quaternion _relativeRotation;
 
     private void Start()
     {
-//        _camera = GetComponent<Camera>();
+        _isMoving = false;
         _relativePosition = transform.position;
+        _radii = new float[2];
+        _radii[0] = _actionRadius;
+        _radii[1] = _stopRadius;
     }
 
     private void Update()
@@ -24,11 +28,17 @@ public class FollowingCamera : MonoBehaviour
         Vector3 currentPosition = transform.position;
         Vector3 targetPosition = _target.position + _relativePosition;
         Vector3 direction = targetPosition - currentPosition;
+        int isMoving = _isMoving ? 1 : 0;
         _distance = direction.magnitude;
-        if (_distance > _stopRadius)
+        if (_distance > _radii[isMoving])
         {
+            _isMoving = true;
             direction.Normalize();
             transform.Translate(direction * _speed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            _isMoving = false;
         }
     }
 }
